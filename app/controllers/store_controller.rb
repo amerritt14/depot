1bcce418265dd45af63f74b9cb81713bbf666675
Andrew Merritt <amerritt14@liberty.edu>
2016-10-27 13:42:21 -0400

    Finished Task D Play-Time

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,11 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    @count = increment_session_counter
+  end
+
+  def increment_session_counter
+    session[:counter] = 0 if session[:counter].nil?
+    session[:counter] += 1
   end
 end
