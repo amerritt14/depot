dc1197cea1ebf2ab003577bb64117fd1fcfb9cd8
Andrew Merritt <amerritt14@liberty.edu>
2016-10-27 12:39:24 -0400

    Finished Task C

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,9 @@
 class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
-#
+
+  validates_length_of :title, :minimum => 10,
+    message: 'must be 10 characters'
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
     with:    %r{\.(gif|jpg|png)\Z}i,
